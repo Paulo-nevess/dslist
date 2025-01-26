1e6849d35fffc3a9c931aac43977a030517448dc
Paulo-nevess <paulohenriqueazulao2pr@gmail.com>
2025-01-25 23:43:33 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,23 +22,27 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private Double scor;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double scor, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
-		this.scor = scor;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -84,12 +88,12 @@ public class Game {
 		this.platforms = platforms;
 	}
 
-	public Double getScor() {
-		return scor;
+	public Double getScore() {
+		return score;
 	}
 
-	public void setScor(Double scor) {
-		this.scor = scor;
+	public void setScore(Double scor) {
+		this.score = scor;
 	}
 
 	public String getImgUrl() {
